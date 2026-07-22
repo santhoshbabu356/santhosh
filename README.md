6675a029c2027a75e47bc7f5b2606aa16b43245f
santhoshbabu356 <santhoshsanthoshb356@gmail.com>
2026-07-22 09:47:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Experiment 1
+## Study of SELinux Modes and Basic Security Commands
+
+### Aim
+To study Linux security basics and verify SELinux modes using CentOS
+
+
+### Expected Output
+
+- SELinux mode displayed
+- SELinux status displayed
+- Permissive mode verified
+- Enforcing mode verified
+- Configuration file displayed
+
+### Result
+
+The SELinux modes were successfully verified using Linux security commands.
---
+++ selinux_lab.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "===== SELinux Practical ====="
+
+echo "Current SELinux Mode"
+//include the command for current mode
+
+echo
+echo "SELinux Status"
+//include the command for status
+
+echo
+echo "Changing to Permissive Mode"
+//include the command for changing the permissive mode
+
+echo
+echo "Current Mode"
+//include the command for current mode
+
+echo
+echo "Changing to Enforcing Mode"
+//include the command for enforcing mode
+
+echo
+echo "Current Mode"
+//include the command for current mode
+
+echo
+echo "Configuration File"
+cat /etc/selinux/config
